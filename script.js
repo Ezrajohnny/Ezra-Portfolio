d625edec203fd2749e89c3ff0359e3bfc4a5e5d0
Ezra <49055182+Ezrajohnny@users.noreply.github.com>
2026-06-16 01:54:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,3 +33,22 @@ content.style.display = "block";
 }
 
 }
+
+const accordions = document.querySelectorAll(".accordion-btn");
+
+accordions.forEach(btn => {
+
+btn.addEventListener("click", () => {
+
+const content = btn.nextElementSibling;
+
+if(content.style.display === "block"){
+content.style.display = "none";
+}
+else{
+content.style.display = "block";
+}
+
+});
+
+});
